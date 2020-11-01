590d5f76bcb51cdfc69396fb83c51db2f4d4b0bb
himbirlasoft <46938624+himbirlasoft@users.noreply.github.com>
2020-11-01 18:28:00 +0530

    Static file changes and new app travelapp

--- travelpro/travelpro/urls.py
+++ travelpro/travelpro/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('calc.urls')),
+    path('',include('travelapp.urls')),
     path('admin/', admin.site.urls),
     
 ]

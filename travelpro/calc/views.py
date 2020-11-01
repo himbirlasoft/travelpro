2ed76cbe352ba6824bcc5e7b07d883c3ffefafa8
himbirlasoft <46938624+himbirlasoft@users.noreply.github.com>
2020-11-01 13:52:56 +0530

    Adding Calc

--- travelpro/calc/views.py
+++ travelpro/calc/views.py
@@ -3,4 +3,11 @@ from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world")
+    return render(request,'home.html',{'name':'Himanshu'})
+
+
+def add(request):
+    val1=int(request.GET["num1"])
+    val2=int(request.GET["num2"])
+    res = val1 + val2
+    return render(request,"result.html",{"result":res})

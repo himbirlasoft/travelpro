11746201671327b708ec200a136e352af9131f59
himbirlasoft <46938624+himbirlasoft@users.noreply.github.com>
2020-11-07 22:41:35 +0530

    data bse settings

--- travelpro/travelapp/views.py
+++ travelpro/travelapp/views.py
@@ -4,24 +4,6 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1= Destination()
-    dest1.name='Mumbai'
-    dest1.desc='Mumbai is the bese place'
-    dest1.price=1100
-    dest1.img='destination_1.jpg'
-
-    dest2= Destination()
-    dest2.name='Delhi'
-    dest2.desc='Delhi is the bese place'
-    dest2.price=1100
-    dest2.img='destination_2.jpg'
-
-    dest3= Destination()
-    dest3.name='Kanpur'
-    dest3.desc='Kanpur is the bese place'
-    dest3.price=1100
-    dest3.img='destination_3.jpg'
-
-    dest=[dest1,dest2,dest3]
+    dest=Destination.objects.all()
 
     return render(request,"index.html",{'dest':dest} )
